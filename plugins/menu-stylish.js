c665c889cd98f8cd4f8d8c93a5f43302362d6fb3
zjhgj <doctorkamran380@gmail.com>
2025-12-16 22:57:10 +0500

    Update menu-stylish.js

--- plugins/menu-stylish.js
+++ plugins/menu-stylish.js
@@ -71,7 +71,7 @@ cmd({
             try {
                 await new Promise(resolve => setTimeout(resolve, 1000)); // Small delay after image
                 await conn.sendMessage(from, {
-                    audio: { url: 'https://files.catbox.moe/5wjnm0.mp3' },
+                    audio: { url: 'https://files.catbox.moe/bo72gy.mp3' },
                     mimetype: 'audio/mp4',
                     ptt: false,
                 }, { quoted: mek });
