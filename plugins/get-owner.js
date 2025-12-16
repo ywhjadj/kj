80ada66259b42246182376d660e05edc5946d880
zjhgj <doctorkamran380@gmail.com>
2025-12-16 22:13:15 +0500

    Update get-owner.js

--- plugins/get-owner.js
+++ plugins/get-owner.js
@@ -57,7 +57,7 @@ async (conn, mek, m, { from }) => {
         await conn.sendMessage(from, {
             audio: { url: 'https://files.catbox.moe/tz8ppu.mp3' }, // Audio URL
             mimetype: 'audio/mp4',
-            ptt: fasle
+            ptt: false
         }, { quoted: mek });
 
     } catch (error) {
