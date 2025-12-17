89d0d4bb6e05b9f38603993d0f754981a68955c5
zjhgj <doctorkamran380@gmail.com>
2025-12-17 07:12:57 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -188,7 +188,7 @@ ${repoData.description || 'No description provided'}
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/bo72gy.mp3' },
+            audio: { url: 'https://files.catbox.moe/etattc.mp3' },
             mimetype: 'audio/mp4',
             ptt: false,
             contextInfo: { 
